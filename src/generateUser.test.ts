c511164ce8f421a0c49d59e5e02d6884654272be
Lukas Oppermann <lukasoppermann@github.com>
2024-09-24 09:42:00 +0200

    rm lib from package.json

--- src/generateUser.test.ts
+++ src/generateUser.test.ts
@@ -2,17 +2,6 @@ import { generateUser } from './generateUser'
 import { userdata } from './data/userdata'
 
 describe('generateUser', () => {
-  it('generates a random users', () => {
-    expect(Object.keys(generateUser(userdata))).toStrictEqual([
-      "name",
-      "login",
-      "avatar_url",
-      "id",
-      "email",
-      "hireable",
-    ])
-  })
-
   it('generates a specific users', () => {
     const selected = userdata[0]
     const user = generateUser(userdata, selected.login)
@@ -28,16 +17,8 @@ describe('generateUser', () => {
     expect(user.avatar_url).toStrictEqual(`https://raw.githubusercontent.com/primer/generated-data/main/assets/users/${selected.login}.png`)
   })
 
-  it('generates a random user if provided user does not exist', () => {
-    const user = generateUser(userdata, 'Homer Simpson')
-    expect(Object.keys(user)).toStrictEqual([
-      "name",
-      "login",
-      "avatar_url",
-      "id",
-      "email",
-      "hireable",
-    ])
-    expect(user.name).not.toEqual("Homer Simpson")
+  it('thows an error if the user does not exist', () => {
+    // @ts-ignore
+    expect(() => generateUser(userdata, 'Homer Simpson')).toThrowError()
   })
 })
